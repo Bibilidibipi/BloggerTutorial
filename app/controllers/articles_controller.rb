340c1f2c5ab6fce8b8a4c4e2f629382d18d36e44
Bibilidibipi <judy0wacker@gmail.com>
2015-03-07 15:35:42 -0800

    added some views and controller methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,26 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
